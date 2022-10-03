e8aef6d1097b54b4effaab7da0ec0d1718a5d4c5
Brijesh Yadav <brijesh.yadav05@ad.infosys.com>
2022-10-03 17:20:51 +0530

    Routing and Navigation

---
+++ routing-demo/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { routingComponents } from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    routingComponents
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
