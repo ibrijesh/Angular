ae20c3771a7712f525a3bed60c702fa814b0bd31
Brijesh Yadav <brijesh.yadav05@ad.infosys.com>
2022-10-05 19:16:09 +0530

    ReactiveForm-  FormBuilder Service

--- reactive-form/src/app/app.component.ts
+++ reactive-form/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormGroup,FormControl} from '@angular/forms'
+import {FormBuilder} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,18 +8,32 @@ import {FormGroup,FormControl} from '@angular/forms'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  registrationForm=new FormGroup({
-    userName:new FormControl('Brijesh'),
-    password:new FormControl(''),
-    confirmPassword:new FormControl(''),
-    address:new FormGroup({
-      city:new FormControl(''),
-      state:new FormControl(''),
-      postalCode:new FormControl('')
+
+  constructor(private fb:FormBuilder) {}
+
+  registrationForm=this.fb.group({
+    userName:['Vishwas'],
+    password:['test'],
+    confirmPassword:['test'],
+    address:this.fb.group({
+      city:[''],
+      state:[''],
+      postalCode:['']
     })
 
-  });
+  }) 
+  
+  // registrationForm=new FormGroup({
+  //   userName:new FormControl('Brijesh'),
+  //   password:new FormControl(''),
+  //   confirmPassword:new FormControl(''),
+  //   address:new FormGroup({
+  //     city:new FormControl(''),
+  //     state:new FormControl(''),
+  //     postalCode:new FormControl('')
+  //   })
+
+  // });
 
 
   loadApiData(){
