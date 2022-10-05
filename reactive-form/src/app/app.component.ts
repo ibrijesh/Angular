b0cc2390c12e7c499318779746b8cb087067f383
Brijesh Yadav <brijesh.yadav05@ad.infosys.com>
2022-10-05 18:13:45 +0530

    Reactive Forms- Creating the Form Model

--- reactive-form/src/app/app.component.ts
+++ reactive-form/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup,FormControl} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-form';
+  
+  registrationForm=new FormGroup({
+    userName:new FormControl('Brijesh'),
+    password:new FormControl(''),
+    confirmPassword:new FormControl(''),
+
+  });
+
 }
